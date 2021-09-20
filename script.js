741b2b858745079f5b3a74678d73774d2747a79c
Nico Koenig <nico.koenig@coding-bootcamps.eu>
2021-09-20 11:55:58 +0200

    add todo to list

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ console.log("Hello Todo App!");
 function addNewTodo() {
   const newTodoEl = document.querySelector("#new-todo");
   const newTodo = newTodoEl.value;
-  console.log("Your new todo: " + newTodo);
+
+  const newTodoLiEl = document.createElement("li");
+  newTodoLiEl.innerText = newTodo;
+
+  const todoListEl = document.querySelector("#todo-list");
+  todoListEl.appendChild(newTodoLiEl);
 }
 
 const addTodoBtn = document.querySelector("#add-todo");
