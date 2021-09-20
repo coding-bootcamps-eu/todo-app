b5b653bd486f24d9312bac78691886000f18b5fd
Nico Koenig <nico.koenig@coding-bootcamps.eu>
2021-09-20 11:26:39 +0200

    read new todo

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello Todo App!");
+
+function addNewTodo() {
+  const newTodoEl = document.querySelector("#new-todo");
+  const newTodo = newTodoEl.value;
+  console.log("Your new todo: " + newTodo);
+}
+
+const addTodoBtn = document.querySelector("#add-todo");
+addTodoBtn.addEventListener("click", addNewTodo);
